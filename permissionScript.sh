5fa1639d32a08fb4bed8e35efe37b9268b110992
Stefan Rinderle <stefan@rinderle.info>
2012-05-27 17:01:28 +1000

    added permission script

---
+++ permissionScript.sh
@@ -0,0 +1,4 @@
+chmod 777 /Users/stefan/Sites/3dArch/protected/data/graph.db
+chmod 777 /Users/stefan/Sites/3dArch/protected/data/
+chmod 777 /Users/stefan/Sites/3dArch/protected/runtime/actualResource.dot
+chmod 777 /Users/stefan/Sites/3dArch/protected/runtime/temp.dot
