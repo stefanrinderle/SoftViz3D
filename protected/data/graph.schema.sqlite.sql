385ac75d3d3cbe1cc9f972c983d08c54b033f10d
Stefan Rinderle <stefan@rinderle.info>
2012-07-19 22:33:43 +1000

    cleanup

--- protected/data/graph.schema.sqlite.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE tbl_EdgeElement (parent_id integer, id INTEGER PRIMARY KEY, label TEXT, out_id integer, in_id integer);
-CREATE TABLE tbl_TreeElement (level integer, id INTEGER PRIMARY KEY, parent_id integer, label TEXT);
